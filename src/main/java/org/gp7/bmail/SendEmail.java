bef0c33d4edc70da88856d4782e52d6f8d3046e6
Derek Avery <derek.avery@bellevuecollege.edu>
2019-06-13 20:43:52 -0700

    modified SendEmail to accept User parameter/email fields; updated corresponding test

--- src/main/java/org/gp7/bmail/SendEmail.java
+++ src/main/java/org/gp7/bmail/SendEmail.java
@@ -2,7 +2,6 @@ package org.gp7.bmail;
 
 
 import java.util.Properties;
-
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -11,10 +10,16 @@ import javax.mail.internet.MimeMessage;
 import javax.swing.JOptionPane;
 
 public class SendEmail {
+	String to, subject, body;
+	User user;
 
-	public SendEmail(String to,String subject, String body) {
+	public SendEmail(User user, String to, String subject, String body) {
+		this.to = to;
+		this.subject = subject;
+		this.body = body;
+		this.user = user;
 		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.host", user.getSmtpServer());
 		props.put("mail.smtp.SocketFactory.port", "587");
 		props.put("mail.smtp.SocketFactory.class", "javax.net.SSL.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
@@ -24,29 +29,27 @@ public class SendEmail {
 		//Maybe put this in another class for full implementation ex: EmailAut(usn,pass)
 		javax.mail.Authenticator auth = new javax.mail.Authenticator() {
 			protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-				return new javax.mail.PasswordAuthentication("group.seven.test@gmail.com", "group7isthebest");
+				return new javax.mail.PasswordAuthentication(user.getEmail(), user.getEmailPassword());
 			}
 		};
+
 		//Using the properties we defined in props, and the authentication details, we define
 		//a session sess.
 		//Once provided with these details, the session will persist as long as the client is logged in.
-		Session sess = Session.getDefaultInstance(props,auth);
+		Session sess = Session.getDefaultInstance(props, auth);
 
 		try {
 			Message message = new MimeMessage(sess);
 			//SetFrom should always be from the currently logged in user.
-			message.setFrom(new InternetAddress("group.seven.test@gmail.com"));
-
-			//message.setHeader(arg0, arg1); I am not sure what this does
-
-			message.setSubject("Hello!");
-			message.setText("Hello! If you are reading this, this actually works :D");
+			message.setFrom(new InternetAddress(user.getEmail()));
+			message.setSubject(subject);
+			message.setText(body);
 
 			//The first parameter is the type of 'sent' -- TO or CC.
 			//The second parameter is the address in the form "username@host.com"
 			//This is defined by the user at runtime.
 			//new InternetAddress(String) or InternetAddress.parse(String)?
-			message.setRecipient(Message.RecipientType.TO, new InternetAddress("jjstanderfer@gmail.com"));
+			message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
 			//message.setRecipients(Message.RecipientType.CC, [[array of InternetAddresses]]); <-For multiple recipients
 
 			Transport.send(message);
--- src/test/java/org/gp7/bmail/EmailTest.java
+++ src/test/java/org/gp7/bmail/EmailTest.java
@@ -2,7 +2,8 @@ package org.gp7.bmail;
 
 public class EmailTest {
 	public static void main(String[] args) {
-		SendEmail send = new SendEmail(null,null,null);
+		User user = new User("group7", "password", "group.seven.test@gmail.com", "group7isthebest", "pop.gmail.com", "smtp.gmail.com");
+		SendEmail send = new SendEmail(user,"averyderekl@gmail.com","test", "test content");
 	}
 
 }
